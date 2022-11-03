44f97fe5b1537bde0fcf53979f44a128620acbac
CyberTML <cybertml@outlook.com>
2022-11-03 09:53:09 -0400

    Updated Login/Logout

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -7,7 +7,22 @@ export default function Categories() {
     <div className="cat-container">
           <div className="cat-body">            
               <div className="cat-header text-center">
-                <h1>Categories</h1>                  
+                <h1>Welcome to React Todo</h1>                  
+              </div>
+              <div className='text-center'>
+                <br />
+                <h4><em>Instructions:</em></h4>
+                <br />
+                <p></p>
+                  1. Login with GitHub to access your account.
+                </p>
+                <p>
+                  2. Access the Todo page to get your checklists started.
+                </p>
+                <p>
+                  3. Add/Remove Lists and Todos the fit your needs.
+                </p>
+                
               </div>             
           </div>          
       </div>
